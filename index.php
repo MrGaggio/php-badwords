51c94c4dc94b5aae9592f8ada88b9124e1f59243
Stefano Ruggiu <steruggiu@gmail.com>
2022-01-28 15:48:53 +0000

    creata struttra e variabile

---
+++ index.php
@@ -0,0 +1,27 @@
+<!-- 
+Descrizione:
+Creare una variabile con un paragrafo di testo a vostra scelta.
+Stampare a schermo il paragrafo e la sua lunghezza.
+Una parola da censurare viene passata dall’utente tramite parametro GET.
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. 
+-->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+        $via = 'nella mia via ci sono 5 case prova'
+    ?>
+  <p>Quante case ci sono nella tua via?</p>
+  <p><?php echo $via ?></p>
+    
+</body>
+</html>
